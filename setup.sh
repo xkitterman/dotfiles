7b237e19be40bc0c7038176d0f95787533b32b89
Jordan Kitterman <xkitterman@gmail.com>
2018-09-05 17:35:56 -0700

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+rm ~/.bashrc
+ln -s $PWD/bashrc ~/.bashrc
+
+rm ~/.gitconfig
+ln -s $PWD/gitconfig ~/.gitconfig
+
+rm ~/.gitignore
+ln -s $PWD/gitignore ~/.gitignore
+
+rm ~/.vimrc
+ln -s $PWD/vimrc ~/.vimrc
+
+rm ~/.gvimrc
+ln -s $PWD/gvimrc ~/.gvimrc
+
+rm ~/.vim
+ln -s $PWD/vim ~/.vim
